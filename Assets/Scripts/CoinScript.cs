81ef2f002c9391ac40391f56469478ba87488eeb
Rafael de Paula <rafael_paula@outlook.com>
2026-06-25 23:19:15 -0300

    refactor: make coin collection safer

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -1,21 +1,55 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CoinScript : MonoBehaviour
 {
+    private const string CoinTag = "Coin";
+
     [SerializeField]
-    Text _text;
+    private Text _text;
+
+    private int _coins;
+
+    private void Awake()
+    {
+        _coins = GetInitialCoinAmount();
+        UpdateCoinText();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Coin")
+        if (!collision.CompareTag(CoinTag))
         {
-            Destroy(collision.gameObject);
-
-            _text.text = Convert.ToString(Convert.ToInt32(_text.text) + 1);
+            return;
         }
+
+        CollectCoin(collision.gameObject);
+    }
+
+    private void CollectCoin(GameObject coin)
+    {
+        _coins++;
+        UpdateCoinText();
+        Destroy(coin);
+    }
+
+    private int GetInitialCoinAmount()
+    {
+        if (_text == null)
+        {
+            return 0;
+        }
+
+        return int.TryParse(_text.text, out int currentCoins) ? currentCoins : 0;
+    }
+
+    private void UpdateCoinText()
+    {
+        if (_text == null)
+        {
+            return;
+        }
+
+        _text.text = _coins.ToString();
     }
 }
