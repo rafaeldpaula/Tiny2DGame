e4222a7c9ae0be63c60dac49a006db7c767825f1
Rafael de Paula <rafael_paula@outlook.com>
2026-06-25 23:19:32 -0300

    refactor: improve camera follow bounds

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,71 +1,130 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraScript : MonoBehaviour
 {
-    Camera _camera;
-    Rect _rect;
+    private Camera _camera;
+    private Rect _rect;
 
     [SerializeField]
-    Transform Player;
-    [SerializeField]
-    BoxCollider2D _boxCollider;
-    [SerializeField]
-    EdgeCollider2D _edgeCollider2;
+    private Transform Player;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private BoxCollider2D _boxCollider;
+
+    [SerializeField]
+    private EdgeCollider2D _edgeCollider2;
+
+    private void Awake()
     {
-        _camera = gameObject.GetComponent<Camera>();
-        _rect = GetWorldBounds(_boxCollider);
+        _camera = GetComponent<Camera>();
+    }
 
-        var points = _edgeCollider2.points;
+    private void Start()
+    {
+        RefreshBounds();
+        UpdateEdgeColliderBounds();
+    }
 
-        points[0] = new Vector2(_rect.xMin + (_rect.xMin / 2f), _rect.yMin + (_rect.yMin / 2f));
-        points[1] = new Vector2(_rect.xMin + (_rect.xMin / 2f), _rect.yMax - (_rect.yMin / 2f));
-        points[2] = new Vector2(_rect.xMax - (_rect.xMin / 2f), _rect.yMax - (_rect.yMin / 2f));
-        points[3] = new Vector2(_rect.xMax - (_rect.xMin / 2f), _rect.yMin + (_rect.yMin / 2f));
-        points[4] = new Vector2(_rect.xMin + (_rect.xMin / 2f), _rect.yMin + (_rect.yMin / 2f));
+    private void LateUpdate()
+    {
+        if (Player == null || _boxCollider == null)
+        {
+            return;
+        }
 
-        _edgeCollider2.points = points;
+        RefreshBounds();
+        transform.position = GetClampedCameraPosition(Player.position);
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = new Color(1.0f, 0.5f, 0.0f);        
-        DrawRect(_rect);
+        if (_boxCollider == null)
+        {
+            return;
+        }
+
+        Gizmos.color = new Color(1f, 0.5f, 0f);
+        DrawRect(GetWorldBounds(_boxCollider));
     }
 
     public Rect GetWorldBounds(BoxCollider2D boxCollider2D)
     {
-        float worldRight = boxCollider2D.transform.TransformPoint(boxCollider2D.offset + new Vector2(boxCollider2D.size.x * 0.5f, 0)).x;
-        float worldLeft = boxCollider2D.transform.TransformPoint(boxCollider2D.offset - new Vector2(boxCollider2D.size.x * 0.5f, 0)).x;
+        if (boxCollider2D == null)
+        {
+            return new Rect();
+        }
 
-        float worldTop = boxCollider2D.transform.TransformPoint(boxCollider2D.offset + new Vector2(0, boxCollider2D.size.y * 0.5f)).y;
-        float worldBottom = boxCollider2D.transform.TransformPoint(boxCollider2D.offset - new Vector2(0, boxCollider2D.size.y * 0.5f)).y;
-
-        return new Rect(
-            worldLeft,
-            worldBottom,
-            worldRight - worldLeft,
-            worldTop - worldBottom
-        );
+        Bounds bounds = boxCollider2D.bounds;
+        return new Rect(bounds.min.x, bounds.min.y, bounds.size.x, bounds.size.y);
     }
 
-    void DrawRect(Rect rect)
+    private void RefreshBounds()
+    {
+        _rect = GetWorldBounds(_boxCollider);
+    }
+
+    private Vector3 GetClampedCameraPosition(Vector3 targetPosition)
+    {
+        if (!_camera.orthographic)
+        {
+            return new Vector3(targetPosition.x, targetPosition.y, transform.position.z);
+        }
+
+        float verticalExtent = _camera.orthographicSize;
+        float horizontalExtent = verticalExtent * _camera.aspect;
+
+        float minX = _rect.xMin + horizontalExtent;
+        float maxX = _rect.xMax - horizontalExtent;
+        float minY = _rect.yMin + verticalExtent;
+        float maxY = _rect.yMax - verticalExtent;
+
+        float x = ClampOrCenter(targetPosition.x, minX, maxX);
+        float y = ClampOrCenter(targetPosition.y, minY, maxY);
+
+        return new Vector3(x, y, transform.position.z);
+    }
+
+    private float ClampOrCenter(float value, float min, float max)
+    {
+        if (min > max)
+        {
+            return (min + max) * 0.5f;
+        }
+
+        return Mathf.Clamp(value, min, max);
+    }
+
+    private void UpdateEdgeColliderBounds()
+    {
+        if (_edgeCollider2 == null || _boxCollider == null)
+        {
+            return;
+        }
+
+        Vector2 bottomLeft = ToEdgeColliderLocal(new Vector2(_rect.xMin, _rect.yMin));
+        Vector2 topLeft = ToEdgeColliderLocal(new Vector2(_rect.xMin, _rect.yMax));
+        Vector2 topRight = ToEdgeColliderLocal(new Vector2(_rect.xMax, _rect.yMax));
+        Vector2 bottomRight = ToEdgeColliderLocal(new Vector2(_rect.xMax, _rect.yMin));
+
+        _edgeCollider2.points = new[]
+        {
+            bottomLeft,
+            topLeft,
+            topRight,
+            bottomRight,
+            bottomLeft
+        };
+    }
+
+    private Vector2 ToEdgeColliderLocal(Vector2 worldPosition)
+    {
+        Vector3 localPosition = _edgeCollider2.transform.InverseTransformPoint(worldPosition);
+        return new Vector2(localPosition.x, localPosition.y);
+    }
+
+    private void DrawRect(Rect rect)
     {
         Gizmos.DrawWireCube(new Vector3(rect.center.x, rect.center.y, 0.01f), new Vector3(rect.size.x, rect.size.y, 0.01f));
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        var rect = GetWorldBounds(_boxCollider);
-
-        var x = Mathf.Clamp(Player.transform.position.x, rect.xMin, rect.xMax);
-        var y = Mathf.Clamp(Player.transform.position.y, rect.yMin, rect.yMax);
-
-        _camera.transform.position = new Vector3(x, y, _camera.transform.position.z);
     }
 }
